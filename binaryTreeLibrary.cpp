a88e4f1d12f0df4d407b1293600d284652f7d0fd
MarthaDs <marthadntas@gmail.com>
2018-11-17 21:45:16 -0200

    [UPDATE] updating funtion loadFromFile

--- binaryTreeLibrary.cpp
+++ binaryTreeLibrary.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
+#include <string.h>
+#define MAX 50
 
 typedef struct tree {
     int value;
@@ -8,52 +9,108 @@ typedef struct tree {
     struct tree *right;
 } Tree;
 
-tree *inicialize();
-void insert(Tree *, int);
-tree *loadTreeFromFile(Tree *, char);
+int showMenu();
+Tree *inicialize();
+Tree *insert(Tree *, int);
+Tree *loadTreeFromFile(Tree *tree, char fileName[MAX]);
+void showTree(Tree *node);
 
 int main() {
+    int opcao = 0;
+    char fileName[MAX];
+    char path[] = "BSTs/";
+    Tree *root = inicialize(); 
+
+    while(opcao != 11) {
+        opcao = showMenu();
+
+        switch(opcao){
+            case 1: 
+                getchar();
+                gets(fileName);
+                strcpy(path,"BSTs/");
+                strcat(path, fileName);
+                printf("%s", path);
+                loadTreeFromFile(root, path);
+                break;
+            
+             case 2:
+                getchar();
+                showTree(root);
+                break;
+            default:
+                printf("Opção inválida\n");
+                break;
+        }
+    }
     return 0;
 }
 
-tree *inicialize() {
-    Tree *tree = (Tree*) malloc(sizeof(Tree));
-    return tree = NULL;
+int showMenu() {
+    int opcao = 0;
+
+    printf("---------------------------------- \n");
+    printf("          Menu de Opções \n");    
+    printf("----------------------------------\n");
+    printf("1 - Load tree from file\n");
+    printf("2 - Show tree\n");
+    printf("3 - Is Full\n");
+    printf("4 - Search value\n");
+    printf("5 - Get height\n");
+    printf("6 - Remove value\n");
+    printf("7 - Print InOrder\n");
+    printf("8 - Print PreOrder\n");
+    printf("9 - Print PosOrder\n");
+    printf("10 - Balance tree\n");
+    printf("11 - Sair\n");
+    printf("\nEscolha uma das opções: \n");
+    scanf("%d", &opcao);
+
+    return opcao;
 }
 
-tree *loadTreeFromFile(Tree *tree, char fileName[MAX]) {
-    Tree *node = (Tree*) malloc(sizeof(Tree));
+Tree *inicialize() {
+    return NULL;
+}
+
+Tree *loadTreeFromFile(Tree *tree, char fileName[MAX]) {
     FILE *file = fopen(fileName,"r");
 
     if(file == NULL) {
-        printf("\nCan't find file");
+        printf("\nCan't find file\n");
     }
     else {
-        node = tree;
         int value;
         while (fscanf(file,"%d ",&value) != EOF) {
-            insert(node,value);
-        }
+            insert(tree,value);
+            if(tree == NULL) printf("sad");
+        } 
     }
 }
 
-void insert(Tree *node, int value) {
+Tree *insert(Tree *node, int value) {
+
     if(node == NULL) {
-        node->value = value;
-        node->left = NULL;
-        node->right = NULL;
+        Tree * newNode = (Tree*) malloc(sizeof(Tree));
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->value = value;
+        node = newNode;
     }
     else if (node->value > value) {
-        insert(node->left,value);
+        node->left = insert(node->left,value);
     }
     else if(node->value < value) {
-        insert(node->right, value);
+        node->right = insert(node->right, value);
     }
+
+    return node;
 }
 
-void showTree(Tree *node) {
-    if(node != NULL) {
-        printf("%d\n", node->value);
+void showTree(Tree *tree) {
+    if(tree != NULL) {
+        printf("%d", tree->value);
+        showTree(tree->left);
+        showTree(tree->right);
     }
- // incompleta
 }
